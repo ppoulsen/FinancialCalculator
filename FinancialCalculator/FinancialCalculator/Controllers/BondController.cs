4e58f8772f99d730fff9d776577b1e2d2ab00b1a
ppoulsen <ppoulsen21@gmail.com>
2013-11-25 22:25:59 -0600

    HTTP GET for Bonds. Gotta go back and rename m though :P

--- FinancialCalculator/FinancialCalculator/Controllers/BondController.cs
+++ FinancialCalculator/FinancialCalculator/Controllers/BondController.cs
@@ -15,6 +15,41 @@ namespace FinancialCalculator.Controllers
         {
             return View();
         }
+        
+        [HttpGet]
+        public ActionResult BondWithPandCandMm(double F, double P, double C, long M, long m)
+        {
+            return Redirect("~/Bond/");
+        }
 
+        [HttpGet]
+        public ActionResult BondWithPandCandN(double F, double P, double C, long N)
+        {
+            return Redirect("~/Bond/");
+        }
+
+        [HttpGet]
+        public ActionResult BondWithPandyandMm(double F, double P, double y, long M, long m)
+        {
+            return Redirect("~/Bond/");
+        }
+
+        [HttpGet]
+        public ActionResult BondWithPandyandN(double F, double P, double y, long N)
+        {
+            return Redirect("~/Bond/");
+        }
+
+        [HttpGet]
+        public ActionResult BondWithCandyandMm(double F, double C, double y, long M, long m)
+        {
+            return Redirect("~/Bond/");
+        }
+
+        [HttpGet]
+        public ActionResult BondWithCandyandN(double F, double C, double y, long N)
+        {
+            return Redirect("~/Bond/");
+        }
     }
 }
