1e7f05648104a79b2b70e0f3ff1c33e122fffe96
ppoulsen <ppoulsen21@gmail.com>
2013-12-12 12:54:11 -0600

    Initial Venture Capital view styling.

---
+++ FinancialCalculator/FinancialCalculator/Controllers/VentureCapitalController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FinancialCalculator.Controllers
+{
+    public class VentureCapitalController : Controller
+    {
+        //
+        // GET: /VentureCapital/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
